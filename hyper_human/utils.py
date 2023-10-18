71dbc38b8539e775339f8c4674ec76380137232e
Masahide Okada <masahideokada1121@gmail.com>
2023-10-18 23:50:50 +0900

    updated average

--- hyper_human/utils.py
+++ hyper_human/utils.py
@@ -1,4 +1,4 @@
-from typing import Tuple, List
+from typing import Tuple, List, Union
 
 import torch
 from torch import Tensor
@@ -11,5 +11,5 @@ def get_mean_and_std(x: Tensor, dim: Tuple[int] = (2, 3)) -> Tuple[Tensor]:
 def normalize(x: Tensor, mean: Tensor, std: Tensor) -> Tensor:
     return (x - mean) / std
 
-def average(inputs: List[Tensor]) -> Tensor:
-    return torch.stack(inputs).sum(dim=0) / len(inputs)
+def average(inputs: List[Tensor], dim: Union[int, Tuple[int]] = 0) -> Tensor:
+    return torch.stack(inputs).sum(dim=dim) / len(inputs)
