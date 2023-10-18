1f6ea228459740d18d408c5bf55bdf187bf40610
Masahide Okada <masahideokada1121@gmail.com>
2023-10-18 23:51:07 +0900

    added SGR modules

--- hyper_human/models/sgr.py
+++ hyper_human/models/sgr.py
@@ -1,1 +1,92 @@
 # Structure Guided Refiner
+
+from typing import List
+
+import torch
+import torch.nn as nn
+from torch import Tensor
+
+from diffusers.configuration_utils import ConfigMixin, register_to_config
+from diffusers.models.modeling_utils import ModelMixin
+
+class SGREncoderConv(nn.Module):
+    def __init__(
+        self,
+        in_channels: int,
+        out_channnels: int,
+        kernel_size: int = 4,
+        stride: int = 2,
+        padding: int = 1
+    ):
+        super().__init__()
+        self.layers = nn.Sequential(
+            nn.Conv2d(in_channels, out_channnels, kernel_size, stride=stride, padding=padding),
+            nn.ReLU()
+        )
+
+    def forward(self, x: Tensor) -> Tensor:
+        out = self.layers(x)
+        return out
+
+class SGREmbedding(nn.Module):
+    def __init__(
+        self,
+        in_channels: int,
+        out_channels: int,
+        num_layers: int = 4,
+        kernel_size: int = 4,
+        stride: int = 2,
+        padding: int = 1
+    ):
+        super().__init__()
+        self.embed = nn.Sequential(
+            SGREncoderConv(in_channels, out_channels, kernel_size, stride=stride, padding=padding),
+            *[
+                SGREncoderConv(
+                    out_channels,
+                    out_channels,
+                    kernel_size,
+                    stride=stride,
+                    padding=padding
+                ) for _ in range(num_layers - 2)
+            ],
+            SGREncoderConv(out_channels, out_channels, 3, stride=1, padding=1),
+        )
+    
+    def forward(self, x: Tensor) -> Tensor:
+        out = self.embed(x)
+        return out
+
+class SGREncoder(ModelMixin, ConfigMixin):
+    @register_to_config
+    def __init__(
+        self,
+        in_channels: int = 3,
+        out_channels: int = 4,
+        num_conditions: int = 3,
+        num_layers: int = 4,
+        kernel_size: int = 4,
+        stride: int = 2,
+        padding: int = 1
+    ):
+        super().__init__()
+        self.num_conditions = num_conditions
+        self.embeds = nn.ModuleList(
+            [
+                SGREmbedding(
+                    in_channels,
+                    out_channels,
+                    num_layers=num_layers,
+                    kernel_size=kernel_size,
+                    stride=stride,
+                    padding=padding
+                ) for _ in range(num_conditions)
+            ]
+        )
+
+    def forward(self, x: List[Tensor]) -> Tensor:
+        if len(x) != self.num_conditions:
+            raise ValueError(f"len(x) must be {self.num_conditions}, but got {len(x)}")
+        samples = [embed(sample) for sample, embed in zip(x, self.embeds)]
+        x_encoded = torch.stack(samples).sum(dim=0)
+        return x_encoded
